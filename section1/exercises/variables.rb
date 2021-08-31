46ff355d1afbf8cdd35d1034d6cf127cd162a383
Robin Dittrich <89048720+Dittrir@users.noreply.github.com>
2021-08-31 16:27:03 -0700

    Update variables.rb

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,8 +1,3 @@
-# In the below exercises, write code that achieves
-# the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
-# `ruby section1/exercises/variables.rb`
-
 # Example: Write code that saves your name to a variable and
 # prints what that variable holds to the terminal:
 name = "Harry Potter"
@@ -11,38 +6,50 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+dobby_thoughts = "Harry Potter must not return to Hogwarts"
+p dobby_thoughts
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
-p students
+p students + 2
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
-p students
+p students - 2
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Robin"
+is_hungry = "very hungry"
+number_of_pets = "0"
+
+p first_name + " is " + is_hungry + " and has " + number_of_pets + " pets."
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
-# Explain.
-
+# I decided to use strings for every variable because it would be easy to insert them
+# with string concatenation, or adding variables together to create a sentence. It was
+# important to note that i needed to add spaces when typing up strings because the
+# variables in between did not have spaces.
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
+
+first_name = "Fred"
+is_hungry = "not hungry"
+number_of_pets = "4"
+
+p first_name + " is " + is_hungry + " and has " + number_of_pets + " pets."
 
 
 # YOU DO:
@@ -50,10 +57,12 @@ p students
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
+total_snacks = 6 + 8
+p "There are " + healthy_snacks.to_s + " healthy snacks and " + junk_food_snacks.to_s + " junk food snacks."
+p "This means there are " + total_snacks.to_s + " total snacks."
 
-#-------------------
-# FINAL CHECK
-#-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+# as you would expect?
+
+p "Yep. Everything came out correctly in my terminal when I ran this file. I had to change the number to a string though."
