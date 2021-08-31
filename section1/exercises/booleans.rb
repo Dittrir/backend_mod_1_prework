637263eb03d58e02afbe93f4eef638585f5428ed
Robin Dittrich <89048720+Dittrir@users.noreply.github.com>
2021-08-31 16:21:48 -0700

    Update booleans.rb

--- section1/exercises/booleans.rb
+++ section1/exercises/booleans.rb
@@ -1,6 +1,3 @@
-#-------------------
-# BOOLEANS
-#-------------------
 
 # EXAMPLE: log to the console the result of 1 is equal to 2:
 p 1 === 2
@@ -9,7 +6,10 @@ p 1 === 2
 p 7 > 2
 
 # YOU DO: log to the console the result of "hello" is equal to "Hello":
+p "hello" === "Hello"
 
 # YOU DO: log to the console the result of 3 is not equal to 4:
+p 3 != 4
 
 # YOU DO: log to the console the result of 4 is less than or equal to 5:
+p 4 <= 5
