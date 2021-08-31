3ee36f9acd70ed3b23a337723f922a5054574e79
Robin Dittrich <89048720+Dittrir@users.noreply.github.com>
2021-08-31 16:15:25 -0700

    Update methods.rb

--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -1,8 +1,3 @@
-# In the below exercises, write code that achieves
-# the desired result. To check your work, run this
-# file by entering the following command in your terminal:
-# `ruby section2/exercises/methods.rb`
-
 # Example: Write a method that when called will print your name:
 def print_name
   p "Severus Snape"
@@ -12,20 +7,35 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  puts name
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method three times with different arguments passed in:
-# YOUR CODE HERE
+def add(a, b)
+  puts "Finding sum of #{a} + #{b}"
+  puts "#{a + b}"
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
-# Then call your method three times with different arguments passed in. 
+end
 
+add(1,3)
+add(4,21)
+add(111,34)
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
+# Then call your method three times with different arguments passed in.
+
+def name_fav_sport(name,sport)
+  puts "#{name}'s favorite sport is #{sport}."
+end
+
+name_fav_sport("John","soccer")
+name_fav_sport("Roger","rugby")
+name_fav_sport("Alice","swimming")
 
 #-------------------
 # PART 3: Naming is Hard
@@ -35,7 +45,12 @@ print_name("Albus Dumbledore")
 # Naming is notoriously hard in programming. It is a skill to name a variable or function concisely enough that it is reasonable to type, but descriptive enough that others can infer the meaning.
 
 # Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
+
+puts "I named the function name_fav_sport because it brings in the name and favorite"
+puts "sport as inputs. That name seems to be descriptive enough on what the method"
+puts "actually does but its not too lenghty for the developer to type."
+
 # What did you name each parameter, and why?
 
-# EXPLAIN:
-
+puts "I named the parameters name and sport because they are collecting those inputs"
+puts "directly from the viewer and using them to concatenate within the method."
