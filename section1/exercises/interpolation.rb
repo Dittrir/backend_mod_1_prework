6637faed47be074a9ce50d94a9f965d010c30cf9
Robin Dittrich <89048720+Dittrir@users.noreply.github.com>
2021-08-31 16:25:19 -0700

    Update interpolation.rb

--- section1/exercises/interpolation.rb
+++ section1/exercises/interpolation.rb
@@ -1,8 +1,3 @@
-# In the below exercises, write code that achieves
-# the desired result. To check your work, run this
-# file by entering the following command in your terminal:
-# `ruby section1/exercises/interpolation.rb`
-
 # Example: Write code that uses the variables below to form a string that reads
 # "The Chudley Cannons are Ron's favorite Quidditch team":
 name = "Ron"
@@ -15,16 +10,20 @@ p "The #{team} are #{name}'s favorite Quidditch team"
 speedy = "quick red fox"
 slow_poke = "lazy brown dog"
 
-p # YOUR CODE HERE
+p "The #{speedy} jumped over the #{slow_poke}"
 
 # Write code that uses the variables below to form a string that reads
 # "In a predictable result, the tortoise beat the hare!":
 slow_poke = "tortoise"
 speedy = "hare"
 
-# YOUR CODE HERE
+p "In a predictable result, the #{slow_poke} beat the #{speedy}!"
 
 
 # YOU DO:
 # Declare three variables, name/content/data type of your choice. Think carefully about what you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!) Then, log out ONE sentence that incorporates all THREE variables.
+not_so_strong_but_smart = "David"
+not_so_smart_but_strong = "Goliath"
+type_of_battle = "single combat"
 
+p "In the biblical book of Samual, the Philistine giant #{not_so_smart_but_strong} was defeated in #{type_of_battle} by the young #{not_so_strong_but_smart}."
